25b71b4bff4f29562e421b4c456f7e311ae2843e
hoyeonyy <highoption@naver.com>
2026-03-09 20:48:34 +0900

    feat: 스켈레톤 코드 생성

---
+++ src/test/java/dongnae_score/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dongnae_score.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
